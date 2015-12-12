07cd162c3a9bf275b02775b3a01d6c7fbea82165
Richard Kiene <richard.kiene@joyent.com>
2015-12-12 13:02:25 -0800

    Initial simple program for gcore samples

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ustack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (;;)
+                ;
+            return;
+        }
+    }
+}
